0e959a4a23e45c927ca1560283c9eedb4090b5d4
Janat Haworth <janat.haworth@utexas.edu>
2017-09-19 00:53:38 -0500

    signals and groups working -> next jobs and job lists

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// Janat Haworth
+// Operating Systems Project 1
+// DUE: September 19, 2017
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -16,12 +20,24 @@ void split(char* input, char** args);
 int isSymbol(char* str);
 int isPipe(char* str);
 void redirect(char* filename);
+void handleRedirection(char** ptr);
 int getfd();
-static void sig_handler(int signo);
+static void sig_int(int signo);
+static void sig_tstp(int signo);
 
-pid_t ch1_pid, ch2_pid, pid;
+
+// yash_pid represents the shell. When a command is entered, 
+// yash will create a command handler with pid cmds_pid which will
+// create ch1 and ch2 if needed for the corresponding commands
+
+// This model was adopted after discovering setsid() in ch1 would not let 
+// ch2 join and would give error EPERM (even in sig_ex3.c) and not being able
+// to figure out why setpgid(0,0) messed with the input/output of pipe neither 
+// by myself nor with the help of Dr. Yeraballi. Thus, he suggested this method. 
+
+pid_t ch1_pid, ch2_pid, yash_pid, cmds_pid;
 char* symbol_ptr;
-char* pipe_ptr = 0; 
+char* pipe_ptr; 
 
 int main () {
 	char str[2000];
@@ -32,14 +48,24 @@ int main () {
 	int pipefd[2];
 	int count = 0; 
 
-	pid = getpid();
+	yash_pid = getpid(); 
+	
+	if (signal(SIGINT, sig_int) == SIG_ERR) {
+		perror("signal(SIGINT) error");
+	}
+	if (signal(SIGTSTP, sig_tstp) == SIG_ERR) {
+		perror("signal(SIGTSTP) error");
+	}
 
 	while(1){ 
+
 		printf("# ");
 		if(fgets(str, 2000, stdin) == NULL){
-			//does not work for children (when written - child would not get here)
-			//printf("exit\n");
-			raise(SIGINT);
+			printf("exit\n");
+			if (cmds_pid > 0 && kill(-cmds_pid, 0) != -1) {
+				kill(-cmds_pid, SIGTERM);
+			}
+			exit(EXIT_SUCCESS);
 		}
 
 		input = trim(str);
@@ -49,129 +75,111 @@ int main () {
 		pipe_ptr = NULL;
 		split(input, args);
 
-		if(pipe_ptr != NULL){
-			if(pipe(pipefd) == -1) {
-				perror("pipe");
-				exit(EXIT_FAILURE);
-			}
-		}
+		cmds_pid = fork();
 
-
-		// fprintf(stderr, "fork 1\n");
-		ch1_pid = fork();
-		if 	(ch1_pid == -1) {
+		if 	(cmds_pid == -1) {
 			perror("fork");
 			exit(EXIT_FAILURE);
 		}
- 
-		if (ch1_pid == 0) {
 
-			if (symbol_ptr != NULL) { 
-
-				if(pipe_ptr != NULL) {
-					close(pipefd[0]); //close read end
-					dup2(pipefd[1], STDOUT_FILENO); //output to pipe
-					close(pipefd[1]);
-				}
-				ptr = args; 
-
-				//find first symbol
-				while(!isSymbol(*ptr)) {
-					ptr++;
-				} 
-
-				while(*ptr != NULL && !isPipe(*ptr)) {
-					symbol_ptr = *ptr;	
-					*ptr = NULL;
-					ptr++;
-					redirect(*ptr);
-
-					//go to next symbol if it exists
-					while(!isSymbol(*ptr) && *ptr != NULL) {
-						ptr++;
-					}	
-				}
-				*ptr = NULL;
+		else if (cmds_pid > 0) {
+			//shell
+			if (waitpid(cmds_pid, &status, 0) == -1) {
+				perror("waitpid");
+				exit(EXIT_FAILURE);
 			}
-
-			execvp(*args, args);
-			fprintf(stderr, "yash: %s: command not found\n", *args);
-			exit(EXIT_FAILURE);	
 		} else {
-			//fprintf(stderr, symbol);
-
-			if(pipe_ptr != NULL) {
-				//code here
-				// fprintf(stderr,"fork 2\n");
-				ch2_pid = fork();
-				if (ch2_pid == 0) {
-					close(pipefd[1]); //close write end
-					dup2(pipefd[0], STDIN_FILENO); // take input from STDIN
-					close(pipefd[0]);
-
-					while (*args != pipe_ptr) {
-						args++;
-					}
-					args++;
-
-					ptr = args;
-					while(!isSymbol(*ptr) && *ptr != NULL) {
-						ptr++;
-					}
-					
-					// fprintf(stderr, *ptr);
-					while(*ptr != NULL) {
-						symbol_ptr = *ptr;	
-						*ptr = NULL;
-						ptr++;
-						//fprintf(stderr, symbol);
-						redirect(*ptr);
-
-						//go to next symbol if it exists
-						while(!isSymbol(*ptr) && *ptr != NULL) {
-							ptr++;
-						}	
-					}
-
-					waitpid(ch1_pid, &status, 0);
-					execvp(*args, args);
-					fprintf(stderr, "yash: %s: command not found\n", *args);
-					exit(EXIT_FAILURE);	
-				}
-				else {
-					close(pipefd[0]);
-					close(pipefd[1]);
-
+			//cmds 
+			if (signal(SIGINT, SIG_DFL) == SIG_ERR) {
+				perror("signal(SIGINT) error");
+			}
+			if (signal(SIGTSTP, SIG_DFL) == SIG_ERR) {
+				perror("signal(SIGINT) error");
+			}
 			
-					if (waitpid(-1, &status, 0) == -1) {
-						perror("waitpid");
-						exit(EXIT_FAILURE);
-					}
+
+			if (setsid() == -1) {
+				perror("setsid");
+			}
+
+			//create pipe
+			if(pipe_ptr != NULL){
+				if(pipe(pipefd) == -1) {
+					perror("pipe");
+					exit(EXIT_FAILURE);
 				}
 			}
 
-			if(getpid() == pid){ 
-				//parent
-				// fprintf(stderr, "parent\n");
+			ch1_pid = fork(); 
 
+			if(ch1_pid == -1) {
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+
+			else if (ch1_pid > 0) {
+				//cmds
+
+				if (pipe_ptr != NULL) {
+					ch2_pid = fork(); 
+
+					if(ch2_pid == -1) {
+						perror("fork");
+						exit(EXIT_FAILURE);
+					} 
+					else if (ch2_pid > 0) {
+						//cmds
+						close(pipefd[0]);
+						close(pipefd[1]);
+
+						if (waitpid(-1, &status, 0) == -1) {
+							perror("waitpid");
+							exit(EXIT_FAILURE);
+						}
+					} else {
+						//child 2
+						close(pipefd[1]); //close write end
+						dup2(pipefd[0], STDIN_FILENO); // take input from STDIN
+						close(pipefd[0]);
+
+						while (*args != pipe_ptr) {
+							args++;
+						}
+						args++;
+						handleRedirection(args);
+						execvp(*args, args);
+						fprintf(stderr, "yash: %s: command/file not found\n", *args);
+						exit(EXIT_FAILURE);	
+					}
+				}
+
+				//cmds
 				if (waitpid(-1, &status, 0) == -1) {
 					perror("waitpid");
 					exit(EXIT_FAILURE);
 				}
 				free(args);
+				exit(EXIT_SUCCESS);
+				
+			} else {
+				//child 1
+				if (symbol_ptr != NULL) { 
+
+					if(pipe_ptr != NULL) {
+						close(pipefd[0]); //close read end
+						dup2(pipefd[1], STDOUT_FILENO); //output to pipe
+						close(pipefd[1]);
+					}
+					handleRedirection(args);
+				}
+
+				execvp(*args, args);
+				fprintf(stderr, "yash: %s: command not found\n", *args);
+				exit(EXIT_FAILURE);	
+
 			}
-			
-//			if(WIFEXITED(status)) {
-//				printf("child done");
-//			} else if(WIFSIGNALED(status)){
-//				printf("signaled");
-//			} else if(WIFSTOPPED(status)){
-//				printf("stopped");
-//			}
 		}
 	}
-	return 0;
-	
 }
 
 char* trim(char* str) {
@@ -209,6 +217,25 @@ int isPipe(char* str) {
 	return (strcmp(str, "|") == 0) ? 1 : 0;
 }
 
+void handleRedirection(char** ptr) {
+	while(!isSymbol(*ptr) && *ptr != NULL) {
+		ptr++;
+	} 
+
+	while(*ptr != NULL && !isPipe(*ptr)) {
+		symbol_ptr = *ptr;	
+		*ptr = NULL;
+		ptr++;
+		redirect(*ptr);
+
+		//go to next symbol if it exists
+		while(!isSymbol(*ptr) && *ptr != NULL) {
+			ptr++;
+		}	
+	}
+	*ptr = NULL;
+}
+
 void redirect(char* filename) {
 	int file = -1;  
 	int fd = getfd();
@@ -227,7 +254,6 @@ void redirect(char* filename) {
 }
 
 int getfd() {
-	//strcmp??
 	if (*symbol_ptr == '<') 
 		return STDIN_FILENO;
 	else if (*symbol_ptr == '>') 
@@ -238,11 +264,20 @@ int getfd() {
 		return -1;
 }
 
-static void sig_handler(int signo) {
-	switch(signo){
-		case SIGINT:
-			//change to -pid to send to group
-		 	kill(ch1_pid, SIGINT);	
-			break;
+static void sig_int(int signo) {
+	if (cmds_pid > 0 && kill(-cmds_pid,0) != -1) {
+		kill(-cmds_pid, SIGINT);
+		printf("\n");
+	}
+	else {
+		//still waiting for input 
+		printf("\n# ");
+	}
+	fflush(stdout);
+}
+
+static void sig_tstp(int signo) {
+	if (cmds_pid > 0 && kill(-cmds_pid,0) != -1) {
+		kill(-cmds_pid, SIGTSTP);
 	}
 }
